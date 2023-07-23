c319114d5734cdbf94409615c8dcac4dec9f56f9
imadeanaccount1 <imadeanaccount@proton.me>
2023-07-22 22:42:10 -0400

    Update Alpha Link

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         {/* Alert */}
         <a
-          href="#"
+          href="https://github.com/imadeanaccount1/wasteof.plus/pulls"
           className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
           role="alert"
         >
--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline">wasteof.plus</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022. Made with ♥ by <a href="https://flowbite.com" className="hover:underline">wasteof.plus</a> using Flowbite.
             </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
