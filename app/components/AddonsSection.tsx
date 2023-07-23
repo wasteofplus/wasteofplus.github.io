ff84dca21b5dbbc4bc5b6db645d9d2a3fabb0cfc
imadeanaccount1 <imadeanaccount@proton.me>
2023-07-23 10:25:56 -0400

    Add Polls Addon

--- app/components/AddonsSection.tsx
+++ app/components/AddonsSection.tsx
@@ -106,6 +106,23 @@ const AddonsSection = () => {
         </svg>
       ),
     },
+    {
+      id: "polls",
+      title: "Polls",
+      description: "Adds polls to wasteof.money.",
+      badge: "Soon!",
+      svg: (
+        <svg
+          className="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 20"
+        >
+          <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v2H7V2ZM5 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm8 4H8a1 1 0 0 1 0-2h5a1 1 0 0 1 0 2Zm0-4H8a1 1 0 0 1 0-2h5a1 1 0 1 1 0 2Z" />
+        </svg>
+      ),
+    },
   ];
   return (
     <section id="addons" className="bg-white dark:bg-gray-900">
