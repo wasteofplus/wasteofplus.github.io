3c0e55d9f8ef4955f14aeab40d515a22e436c3b2
imadeanaccount1 <imadeanaccount@proton.me>
2023-07-23 09:30:31 -0400

    Fix Build Error

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -31,7 +31,7 @@ const NavBar = () => {
         console.log('computed style map: ', localStorage.getItem("color-theme") === "light")
         themeToggleDarkIcon.style.display == ((localStorage.getItem("color-theme") === "light") ? "block" : "none") //.toggle("hidden");
         themeToggleLightIcon.style.display == ((localStorage.getItem("color-theme") === "dark") ? "block" : "none") // .classList.toggle("hidden");
-        if (localStorage.getItem("color-theme") === "light")
+        if (localStorage.getItem("color-theme") === "light") {
             
         }
 
